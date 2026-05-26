2331a6903f32bed078b20a9fa7eeda7c7dfaddbb
Vita Arbaiter <vitaarbajter@gmail.com>
2026-05-26 23:22:20 +0000

    Photo

--- model_logistic_regression.py
+++ model_logistic_regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
 
 # 1. Завантажуємо оброблені дані
 from data_loader import load_and_preprocess_data
@@ -16,3 +17,17 @@ output = pd.DataFrame({'PassengerId': passenger_ids, 'Survived': predictions})
 output.to_csv('submission_logistic_regression.csv', index=False)
 
 print("Готово! Файл submission_logistic_regression.csv успішно створено.")
+
+
+# 4. Візуалізація: вплив факторів на виживання
+print("Малюємо графік...")
+coefficients = model_lr.coef_[0]
+
+plt.figure(figsize=(10, 6), dpi=300)
+# Малюємо горизонтальні стовпчики
+plt.barh(X_train.columns, coefficients, color='mediumpurple')
+plt.title("Вплив факторів на виживання (Логістична регресія)", fontsize=16)
+plt.xlabel("Вага фактора (більше 0 = плюс до виживання, менше 0 = мінус)")
+plt.axvline(x=0, color='grey', linestyle='--') # Додаємо нульову лінію для наочності
+plt.savefig('logistic_regression_importance.png', bbox_inches='tight')
+print("Графік logistic_regression_importance.png збережено!")
